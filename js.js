4d83066759abf6ef75a3edb0851b0e2b72841eb7
Nikita <100701121+NikitaPich@users.noreply.github.com>
2022-03-30 13:40:35 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,28 @@
+let admin
+let name = 'John'
+let some
+admin = null
+
+// admin = prompt('Write Name', '12345')
+console.log(admin)
+console.log(`Hi ${name}`)
+console.log(typeof name)
+
+true
+false
+// false
+true
+false
+false
+false
+
+console.log('a' < 'A')
+
+let lv_number = prompt('Number?', 0)
+if (lv_number > 0) {
+  alert('1')
+} else if (lv_number < 0) {
+  alert('-1')
+} else {
+  alert('0')
+}
